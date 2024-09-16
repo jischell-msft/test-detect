8ce130fea55eac4a1ae71e2654f40aa19348be09
J Schell <jischell-msft@users.noreply.github.com>
2024-09-16 11:46:40 -0700

    add single quote for taskrun, update arg_delim to switch

--- Public/New-Test.SchTasks.ps1
+++ Public/New-Test.SchTasks.ps1
@@ -6,9 +6,9 @@ Generate a new scheduled task. Designed to test detections.
 .NOTES
 Name:       New-Test.SchTasks
 Created:    2024-09-05
-Modified:   2024-09-10
+Modified:   2024-09-16
 Author:     JiSchell
-Version:    0.1.7
+Version:    0.1.8
 
 #>
 
@@ -24,6 +24,10 @@ Version:    0.1.7
         $ArgumentDelimiter = 'All',
 
         [Parameter()]
+        [string]
+        $Payload = "Default",
+
+        [Parameter()]
         [ValidateSet('Upper', 'Lower', 'TitleCase', 'Mix')]
         $Casing = 'Mix'
     )
@@ -35,45 +39,45 @@ Version:    0.1.7
             $sys_dir = @( $($Arch) )
         }
 
-        if ( $ArgumentDelimiter -eq 'All') {
-            $arg_inp = Get-Random -InputObject @('-', '/')
+        switch ($ArgumentDelimiter) {
+            "Dash" { $arg_inp = '-' }
+            "Slash" { $arg_inp = '/' }
+            Default { $arg_inp = Get-Random -InputObject @('-', '/') }
         }
-        elseif ($ArgumentDelimiter -eq 'Dash') {
-            $arg_inp = '-'
-        }
-        else {
-            $arg_inp = '/'
-        }
+
 
         $dateTime_str = (Get-Date -Format s).Replace(':', '_')
         $task_name = "SecTest_$($dateTime_str)"
         $task_time = New-HourTime
         
-        $executables = @(
-            "arp.exe"
-            "calc.exe"
-            "clip.exe"
-            "cmd.exe"
-            "findstr.exe"
-            "mmc.exe"
-            "net.exe"
-            "notepad.exe"
-            "osk.exe"
-            "ping.exe"
-            "reg.exe"
-            "whoami.exe"
-        )
-        $exe_target = $executables[(Get-Random -Minimum 0 -Maximum ($executables.count - 1))]
-
-        $sys_path = Get-Random -InputObject $sys_dir
-
-        $exe_full = "$($env:windir)\$($sys_path)\$($exe_target)"
+        if ( $Payload -eq "Default") {
+            $executables = @(
+                "arp.exe"
+                "calc.exe"
+                "clip.exe"
+                "cmd.exe"
+                "findstr.exe"
+                "mmc.exe"
+                "net.exe"
+                "notepad.exe"
+                "osk.exe"
+                "ping.exe"
+                "reg.exe"
+                "whoami.exe"
+            )
+            $exe_target = $executables[(Get-Random -Minimum 0 -Maximum ($executables.count - 1))]
+            $sys_path = Get-Random -InputObject $sys_dir
+            $exe_full = "$($env:windir)\$($sys_path)\$($exe_target)"
+        }
+        else {
+            $exe_full = $Payload
+        }
 
         $schTask = Get-Random -InputObject @('schtasks', 'schtasks.exe')
     }
     process {
         $command_out = @"
-$($schTask) $($arg_inp)Create $($arg_inp)SC Once $($arg_inp)TN $($task_name) $($arg_inp)TR $($exe_full) $($arg_inp)ST $($task_time);
+$($schTask) $($arg_inp)Create $($arg_inp)SC Once $($arg_inp)TN $($task_name) $($arg_inp)ST $($task_time) $($arg_inp)TR `'$($exe_full)`';
 Start-Sleep -Seconds 5;
 $($schTask) $($arg_inp)Delete $($arg_inp)TN $($task_name) $($arg_inp)F;
 "@
